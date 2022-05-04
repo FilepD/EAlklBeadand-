32f1ccb560a7bf255ab65ceda8ccdf86affb309c
fablevi <fablevi1234@gmail.com>
2022-05-04 19:12:24 +0200

    Main, Rendezveny, Eloado class hozzáadása (init)

--- bead/src/main/java/com/mycompany/bead/Rendezveny.java
+++ bead/src/main/java/com/mycompany/bead/Rendezveny.java
@@ -4,10 +4,52 @@
  */
 package com.mycompany.bead;
 
-/**
- *
- * @author Fábián Levente
- */
+import java.util.ArrayList;
+
+
+
 public class Rendezveny {
+    //private Eloado e = new Eloado();
+    private ArrayList <Eloado> e=new ArrayList<Eloado>();
+    private String helyszin,nev,datum;
+    
+    public Rendezveny(String helyszin, String nev, String datum) {
+        this.helyszin = helyszin;
+        this.nev = nev;
+        this.datum = datum;
+    }
+/*getter*/
+
+    public ArrayList<Eloado> getEloado() {
+        return e;
+    }
+    
+    public String getHelyszin() {
+        return helyszin;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+/*setter*/ 
+    public void setRendezvenyEloado(Eloado e){
+        this.e.add(e);
+    }
+
+    public void setHelyszin(String helyszin) {
+        this.helyszin = helyszin;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
     
 }
