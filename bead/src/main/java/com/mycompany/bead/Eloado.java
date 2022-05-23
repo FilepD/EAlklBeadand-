1d3409febbe15a318491b0a7dbf18e5d19d67830
FilepD <filepdani97@gmail.com>
2022-05-23 21:41:59 +0200

    ember osztály, beszél interface

--- bead/src/main/java/com/mycompany/bead/Eloado.java
+++ bead/src/main/java/com/mycompany/bead/Eloado.java
@@ -8,7 +8,7 @@ package com.mycompany.bead;
  *
  * @author Fábián Levente
  */
-public class Eloado {
+public class Eloado implements Beszel {
     private String név;
     private int időtartam; // percben
     private String téma;
@@ -18,7 +18,7 @@ public class Eloado {
         this.időtartam = időtartam;
         this.téma = téma;
     }
-
+        
     public String getNév() {
         return név;
     }
@@ -39,6 +39,10 @@ public class Eloado {
     public void setTéma(String téma) {
         this.téma = téma;
     }
-    
-    
+    public void kiabál(){
+        System.out.println("");
+    }
+    public void motyog(){
+        System.out.println("");
+    }
 }
---
+++ bead/src/main/java/com/mycompany/bead/Ember.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bead;
+
+/**
+ *
+ * @author user
+ */
+public class Ember {
+    String szín;
+    int súly;
+
+    public Ember(String szín, int súly) {
+        this.szín = szín;
+        this.súly = súly;
+    }
+
+    public String getSzín() {
+        return szín;
+    }
+
+    public void setSzín(String szín) {
+        this.szín = szín;
+    }
+
+    public int getSúly() {
+        return súly;
+    }
+
+    public void setSúly(int súly) {
+        this.súly = súly;
+    }
+    
+    
+    
+}
+
